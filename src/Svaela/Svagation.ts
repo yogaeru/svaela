602606ae931d32598964a23c1e3fee565b63aa2b
yogaeru <yogaadhip3@gmail.com>
2026-02-08 21:46:50 +0700

    feat: support nested branch and normalize path

--- src/Svaela/Svagation.ts
+++ src/Svaela/Svagation.ts
@@ -3,18 +3,40 @@ import type { BranchRoute, Branch, BranchView } from './types';
 export class Svagation {
   private branches: Branch = {};
   
+  getBranches() {
+     return this.branches;
+   }
+  
   branch(route: BranchRoute) {
-    const { path, ...elements } = route;
-    this.branches[path] = elements;
+    this.addBranches(route);
+    
     return this;
   }
   
-  getBranches() {
-    return this.branches;
+  private addBranches(route: BranchRoute, parentPath= '') {
+    const { path, view, children } = route;
+    const fullPath: string = this.concatPath(parentPath, path);
+    // console.log('Parent Path: ', parentPath);
+    // console.log('Child Path: ', path);
+    // console.log('Full Path: ', fullPath);
+    
+    this.branches[fullPath] = view;
+    
+    if (children) {
+      for (const childRoute of children) {
+        this.addBranches(childRoute, fullPath);
+      }
+    }
   }
-}
-
-
-
-
-
+  
+  private concatPath(parentPath: string, childPath: string) {
+    return (
+      '/' +
+      [parentPath, childPath]
+        .filter(Boolean)
+        .map((path: string): string => path.replace(/^\/+|\/+$/, ''))
+        .filter(Boolean)
+        .join('/')
+    )
+  }
+ }
