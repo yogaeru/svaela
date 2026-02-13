76678d9f598944652896f57334143d2a24bf6dbb
yogaeru <yogaadhip3@gmail.com>
2026-02-13 15:10:47 +0700

    chore: add type view element function

--- src/Svaela/types.ts
+++ src/Svaela/types.ts
@@ -1,9 +1,15 @@
+import type { VNode } from "../modules/node/Node";
+
+export type ViewElement = () => HTMLElement | VNode | string;
+
+
 export type BranchRoute = {
-  path: string,
-  view: HTMLElement | string,
-  children?: BranchRoute[]; 
-}
+  path: string;
+  view: ViewElement;
+  children?: BranchRoute[];
+};
 
-export type BranchView = Omit<BranchRoute, 'path'>;
+// export type BranchView = Omit<BranchRoute, "path">;
 
-export type Branch = Record<string, string | HTMLElement>;
+export type Branch = Record<string, () => string | HTMLElement | VNode>;
+ 
