0b70facee7fff24935bfdec0ca7f3b67405d8dd0
yogaeru <yogaadhip3@gmail.com>
2026-02-24 12:55:01 +0700

    update export reactivity helpers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
-import { Svaela } from "./Svaela/Svaela";
+import { createSignal, effect } from "./elements/reactivity";
 import { Svagation } from "./Svaela/Svagation";
+import { Svaela } from "./Svaela/Svaela";
 import { t } from "./elements/t";
 
-export { Svaela, Svagation, t };
+export {
+  t,
+  effect,
+  Svaela,
+  Svagation,
+  createSignal,
+};
